d460d2c107a25661e77360b2ca650534a5759d9a
El-gibbor <chiagoziem11@yahoo.com>
2024-02-05 10:01:17 +0400

    corrected searchbar toggle behaviour and achieved a persistent page navigation

--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import 'boxicons/css/boxicons.min.css';
 import mainlogo from '/src/assets/logo-RN.png';
+import { Link } from 'react-router-dom';
 
 const navLinks = [
-    { icon: 'bx bx-grid-alt', text: 'Dashboard', link: '/Dashboard' },
-    { icon: 'bx bxs-graduation', text: 'Student/Classes', link: '/Students' },
-    { icon: 'bx bx-bar-chart-square', text: 'Exams', link: '/Exam' },
-    { icon: 'bx bx-user', text: 'Teachers', link: '/Teachers' },
-    { icon: 'bx bx-cog', text: 'Settings/profile', link: '/Settings' },
+    { icon: 'bx bx-grid-alt', text: 'Dashboard', path: '/Dashboard' },
+    { icon: 'bx bxs-graduation', text: 'Student/Classes', path: '/Students' },
+    { icon: 'bx bx-bar-chart-square', text: 'Exams', path: '/Exam' },
+    { icon: 'bx bx-user', text: 'Teachers', path: '/Teachers' },
+    { icon: 'bx bx-cog', text: 'Settings/profile', path: '/Settings' },
 ];
 
 function SideBar() {
@@ -17,7 +18,7 @@ function SideBar() {
     // Sidebar toggle
     const toggleSidebar = () => {setOpen(!open)};
 
-    // Search bar toggle
+    // Search bar
     const toggleSearchBar = () => {
         if (!open)
             setOpen(!open)
@@ -46,16 +47,16 @@ function SideBar() {
                 </div>
                 <div className='flex-1 mt-5 space-y-2 font-normal font-sans'>
                     {/* Map through navLinks objs & render dynamically */}
-                    {navLinks.map((link, index) => (
+                    {navLinks.map((navMenuLink, index) => (
                         <li key={index} className={`group flex items-center hover:bg-cyan-600 rounded-xl p-2 ${!open ? 'hover:bg-cyan-600 pl-4 -ml-3 -mr-2.5 rounded-xl' : 'hover:bg-cyan-600 pl-4 -ml-3 -mr-2.5 rounded-xl'}`} >
-                            <a href={link.link} className="flex items-center">
-                                <i className={link.icon + ' text-white'}></i>
-                                <span className={`ml-2 pl-1 text-white ${!open && "hidden"}`}>{link.text}</span>
+                            <Link to={navMenuLink.path} className="flex items-center">
+                                <i className={navMenuLink.icon + ' text-white'}></i>
+                                <span className={`ml-2 pl-1 text-white ${!open && "hidden"}`}>{navMenuLink.text}</span>
                                 {/* Tooltip - popup text on mouse hover */}
                                 {!open ? (
-                                    <span className={`tooltip ml-7 cursor-default text-black font-serif text-xs font-thin bg-cyan-500 rounded p-1 px-2 opacity-0 invisible scale-0 transition-transform transform group-hover:opacity-100 group-hover:visible group-hover:scale-100`} onClick={(e) => e.preventDefault()}>{link.text}</span>
+                                    <span className={`tooltip ml-7 cursor-default text-black font-serif text-xs font-thin bg-cyan-500 rounded p-1 px-2 opacity-0 invisible scale-0 transition-transform transform group-hover:opacity-100 group-hover:visible group-hover:scale-100`} onClick={(e) => e.preventDefault()}>{navMenuLink.text}</span>
                                 ) : null}
-                            </a>
+                           </Link>
                         </li>
                     ))}
                 </div>
