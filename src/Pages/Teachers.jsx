dfd6e940e919668d94742f7d88be18c5d07b16a6
El-gibbor <chiagoziem11@yahoo.com>
2024-02-05 11:57:49 +0400

    correct default behaviour during navigation & nomore event on tooltip

--- src/Pages/Teachers.jsx
+++ src/Pages/Teachers.jsx
@@ -5,7 +5,7 @@ function Teachers() {
     return (
         <Layout>
             <section className='mt-16 font-sans font-bold'>
-                <h1>Teachers</h1>
+                <h1>Your School Teachers</h1>
             </section>
         </Layout>
     );
--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -14,20 +14,14 @@ const navLinks = [
 function SideBar() {
 
     const [open, setOpen] = useState(true);
+    const sidebarWidth = open ? 'w-56' : 'w-16';
 
     // Sidebar toggle
-    const toggleSidebar = () => {setOpen(!open)};
-
-    // Search bar
-    const toggleSearchBar = () => {
-        if (!open)
-            setOpen(!open)
-    };
+    const toggleSidebar = () => { setOpen(!open) };
 
     return (
-        <div className='flex'>
-            <div
-                className={`${open ? 'w-56' : 'w-16'} p-5 h-screen duration-700 bg-blue-950 relative`}
+        <>
+            <div className={`${sidebarWidth} p-5 h-screen duration-700 bg-blue-950 relative `}
             ><i
                 // menu icon changes when the side bar is toggled open or close
                 className={`bx ${open ? 'bx-menu-alt-right' : 'bx-menu'} absolute mt-2 text-white ${open ? 'top-4 right-4' : 'top-4 right-6'} cursor-pointer`}
@@ -35,16 +29,15 @@ function SideBar() {
             ></i>
                 {/* Main logo on Sidebar */}
                 <div className={`w-24 mt-2 ml-auto mr-auto ${!open && "scale-0"}`}>
-                    <img src={mainlogo} alt="" />
+                    <img src={mainlogo} alt="AppLogo" />
                 </div>
                 {/* Search input */}
-                <div className="relative font-normal text-sm  mt-6 -ml-3 -mr-2">
+                <div className="relative font-normal text-sm  mt-9 -ml-3 -mr-2">
                     <i className={`bx bx-search absolute pointer-events-none top-3 text-white ${!open ? 'right-3.5' : 'right-3'}`}></i>
-                    <input
-                        type="text"
-                        placeholder="Search.."
-                        className={`bg-sky-900 caret-cyan-600 p-2 placeholder:pl-8 w-full outline-none rounded-xl px-2 ${!open ? 'p-2 pr-4 right-0' : ''}`} onClick={toggleSearchBar} />
+                    <input type="text" placeholder="Search.."
+                        className={`bg-sky-900 caret-cyan-600 p-2 placeholder:pl-8 w-full rounded-xl px-2 ${!open ? 'p-2 pr-4 right-0' : ''}`} onClick={() => { if (!open) toggleSidebar(); }}/>
                 </div>
+                {/* Sidebar Navlinks */}
                 <div className='flex-1 mt-5 space-y-2 font-normal font-sans'>
                     {/* Map through navLinks objs & render dynamically */}
                     {navLinks.map((navMenuLink, index) => (
@@ -52,16 +45,16 @@ function SideBar() {
                             <Link to={navMenuLink.path} className="flex items-center">
                                 <i className={navMenuLink.icon + ' text-white'}></i>
                                 <span className={`ml-2 pl-1 text-white ${!open && "hidden"}`}>{navMenuLink.text}</span>
-                                {/* Tooltip - popup text on mouse hover */}
+                                {/* Tooltip - hover text when sidebar is closed */}
                                 {!open ? (
                                     <span className={`tooltip ml-7 cursor-default text-black font-serif text-xs font-thin bg-cyan-500 rounded p-1 px-2 opacity-0 invisible scale-0 transition-transform transform group-hover:opacity-100 group-hover:visible group-hover:scale-100`} onClick={(e) => e.preventDefault()}>{navMenuLink.text}</span>
                                 ) : null}
-                           </Link>
+                            </Link>
                         </li>
                     ))}
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
