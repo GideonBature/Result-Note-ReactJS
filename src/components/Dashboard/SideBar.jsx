b23b7aac033876765f394febde10ddd41a84317c
El-gibbor <chiagoziem11@yahoo.com>
2024-02-05 09:45:28 +0400

    tooltip no more interactive - set cusor to default

--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import 'boxicons/css/boxicons.min.css';
 import mainlogo from '/src/assets/logo-RN.png';
-// import DashboardContent from './Content';
 
 const navLinks = [
     { icon: 'bx bx-grid-alt', text: 'Dashboard', link: '/Dashboard' },
@@ -15,6 +14,15 @@ function SideBar() {
 
     const [open, setOpen] = useState(true);
 
+    // Sidebar toggle
+    const toggleSidebar = () => {setOpen(!open)};
+
+    // Search bar toggle
+    const toggleSearchBar = () => {
+        if (!open)
+            setOpen(!open)
+    };
+
     return (
         <div className='flex'>
             <div
@@ -22,39 +30,35 @@ function SideBar() {
             ><i
                 // menu icon changes when the side bar is toggled open or close
                 className={`bx ${open ? 'bx-menu-alt-right' : 'bx-menu'} absolute mt-2 text-white ${open ? 'top-4 right-4' : 'top-4 right-6'} cursor-pointer`}
-                onClick={() => setOpen(!open)}
+                onClick={toggleSidebar}
             ></i>
                 {/* Main logo on Sidebar */}
                 <div className={`w-24 mt-2 ml-auto mr-auto ${!open && "scale-0"}`}>
                     <img src={mainlogo} alt="" />
                 </div>
-                <ul className="flex-1 mt-8 space-y-8 font-normal font-sans">
-                    {/* Search input */}
-                    <li className="relative font-normal text-sm mb-4 -ml-3 -mr-2">
-                        <i className={`bx bx-search absolute top-3 text-white ${!open ? 'right-3.5' : 'right-3'}`}></i>
-                        <input
-                            type="text"
-                            placeholder="Search.."
-                            className={`bg-sky-900 caret-cyan-600 p-2 placeholder:pl-8 w-full outline-none rounded-xl px-2 ${!open ? 'p-2 pr-4 right-0' : ''}`} onClick={() => setOpen(!open)}
-                        />
-                    </li>
+                {/* Search input */}
+                <div className="relative font-normal text-sm  mt-6 -ml-3 -mr-2">
+                    <i className={`bx bx-search absolute pointer-events-none top-3 text-white ${!open ? 'right-3.5' : 'right-3'}`}></i>
+                    <input
+                        type="text"
+                        placeholder="Search.."
+                        className={`bg-sky-900 caret-cyan-600 p-2 placeholder:pl-8 w-full outline-none rounded-xl px-2 ${!open ? 'p-2 pr-4 right-0' : ''}`} onClick={toggleSearchBar} />
+                </div>
+                <div className='flex-1 mt-5 space-y-2 font-normal font-sans'>
                     {/* Map through navLinks objs & render dynamically */}
                     {navLinks.map((link, index) => (
-                        <li
-                            key={index}
-                            className={`group flex items-center hover:bg-cyan-600 rounded-xl p-2 ${!open ? 'hover:bg-cyan-600 pl-4 -ml-3 -mr-2.5 rounded-xl' : 'hover:bg-cyan-600 pl-4 -ml-3 -mr-2.5 rounded-xl'}`}
-                        >
+                        <li key={index} className={`group flex items-center hover:bg-cyan-600 rounded-xl p-2 ${!open ? 'hover:bg-cyan-600 pl-4 -ml-3 -mr-2.5 rounded-xl' : 'hover:bg-cyan-600 pl-4 -ml-3 -mr-2.5 rounded-xl'}`} >
                             <a href={link.link} className="flex items-center">
                                 <i className={link.icon + ' text-white'}></i>
                                 <span className={`ml-2 pl-1 text-white ${!open && "hidden"}`}>{link.text}</span>
-                                {/* Tooltip - mouse hover popup text beside icon when sidebar is closed */}
+                                {/* Tooltip - popup text on mouse hover */}
                                 {!open ? (
-                                    <span className={`tooltip ml-7 text-black text-xs font-serif font-thin bg-cyan-500 rounded p-1 px-2 pointer-events-none opacity-0 invisible scale-0 transition-transform transform group-hover:opacity-100 group-hover:visible group-hover:scale-100`}>{link.text}</span>
+                                    <span className={`tooltip ml-7 cursor-default text-black font-serif text-xs font-thin bg-cyan-500 rounded p-1 px-2 opacity-0 invisible scale-0 transition-transform transform group-hover:opacity-100 group-hover:visible group-hover:scale-100`} onClick={(e) => e.preventDefault()}>{link.text}</span>
                                 ) : null}
                             </a>
                         </li>
                     ))}
-                </ul>
+                </div>
             </div>
         </div>
     );
