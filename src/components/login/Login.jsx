fbeb987003ef1bce3cfa78632bd928cac8a97b58
GideonBature <infoaboutgideon@gmail.com>
2024-02-05 15:48:24 +0100

    add login and register base code

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,53 @@
+// File: /home/bene/Desktop/ResultNote/Result-Note-ReactJS/src/components/login/Login.jsx
+
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome, Log in to your account</h2>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email address
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="mt-1 p-3 block w-full rounded-md border-gray-300"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="mt-1 p-3 block w-full rounded-md border-gray-300"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Log in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
