de8ef6828af36ce8c9babbca64d0c3abaecd0309
El-gibbor <chiagoziem11@yahoo.com>
2024-02-06 01:11:22 +0400

    implemented top navBar, route profile btn to profile/settings page

--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -43,7 +43,7 @@ function SideBar() {
                         <li key={index} className={`group flex items-center hover:bg-secondaryColor rounded-xl p-2 ${!open ? 'hover:bg-secondaryColor pl-4 -ml-3 -mr-2.5 rounded-xl duration-700' : 'hover:bg-secondaryColor pl-4 -ml-3 -mr-2.5 rounded-xl'}`} >
                             <Link to={navMenuLink.path} className="flex items-center">
                                 <i className={navMenuLink.icon + ' text-white'}></i>
-                                <span className={`ml-2 pl-1 duration-700 text-white ${!open && "hidden"}`}>{navMenuLink.text}</span>
+                                <span className={`ml-2 pl-1 duration-700 text-white ${!open && "hidden"}`} >{navMenuLink.text}</span>
                                 {/* Tooltip - hover text when sidebar is closed */}
                                 {!open ? (
                                     <span className={`tooltip ml-7 cursor-default text-black text-xs font-semibold bg-secondaryColor rounded p-1 px-2 opacity-0 invisible scale-0 transition-transform transform group-hover:opacity-100 group-hover:visible group-hover:scale-100`} onClick={(e) => e.preventDefault()}>{navMenuLink.text}</span>
--- src/components/Dashboard/TopNavBar.jsx
+++ src/components/Dashboard/TopNavBar.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const navBarLinks = [
+    { name: 'Profile', path: '/Settings' },
+    { name: 'Logout', path: '#' }
+];
 
 function TopNavBar() {
     return (
         <div className='flex justify-between items-center mt-5'>
             <h1 className="ml-24 text-3xl font-bold text-white">Welcome to Dashboard, Udemy School</h1>
             <ul className="flex text-xs font-semibold text-white mr-6">
-                <li className="mr-4">
-                    <button className="bg-secondaryColor px-6 py-1.5 rounded">Profile</button>
-                </li>
-                <li>
-                    <button className="bg-secondaryColor px-6 py-1.5 rounded">Logout</button>
-                </li>
+                {navBarLinks.map((navLink, index) => (
+                    <li key={index} className='mr-4'>
+                        <Link to={navLink.path}>
+                            <button className="bg-secondaryColor hover:bg-sky-700 px-6 py-1.5 rounded">{navLink.name}</button>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </div>
     );
