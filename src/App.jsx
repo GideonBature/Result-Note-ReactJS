5bf2bc185932a1432da2e708edf7d901887a6991
GideonBature <infoaboutgideon@gmail.com>
2024-02-07 05:31:31 +0100

    complete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import Home from './components/landing_page/Home';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} /> 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
---
+++ src/components/landing_page/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const Home = () => {
+
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
+}
+
+export default Home;
