53a812ed10cf063e96356aac51e23be75f6cf381
wrightkhlebisol <calebogundiya@gmail.com>
2024-02-26 16:16:30 +0000

    Students profile components

--- src/Pages/Students.jsx
+++ src/Pages/Students.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import StudentTable from '../components/StudentResults/StudentTable.jsx';
+import Layout from '../components/Layout.jsx';
+import StudentData from '../components/students/StudentData.jsx';
+
 
 function Students() {
+    const columnNames = [
+        'Name',
+        'Student ID',
+        'Email Address',
+        'Class',
+        'Gender',
+        'Bulk'
+    ];
+
+    const contents = [
+        {
+            details: {
+                name: 'Gideon Bature',
+                src: '/src/assets/gideon.jpeg'
+            },
+            id: 3245,
+            email: 'caleb@gmail.com',
+            class: 'SS2',
+            gender: 'male',
+            bulk: true,
+        },
+        {
+            details: {
+                name: 'Gideon Bature',
+                src: '/src/assets/gideon.jpeg'
+            },
+            id: 3246,
+            email: 'caleb@gmail.com',
+            class: 'SS2',
+            gender: 'male',
+            bulk: true,
+        }
+    ]
+
     return (
         <Layout>
             <section className='mt-16 font-sans font-bold text-black'>
-                <StudentTable />
+                <StudentData headings={columnNames} contents={contents}  />
             </section>
         </Layout>
     );
---
+++ src/components/students/StudentData.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import StudentProfile from './StudentProfile';
+import Gideon from '/src/assets/gideon.jpeg'
+
+import Table from '../table/Table';
+
+
+const StudentData = ({headings, contents}) => {
+    return (
+        <div className="flex">
+            <div className="mr-8 flex-1 overflow-x-auto">
+                <Table headings={headings} contents={ contents} />
+            </div>
+            {/* Student profile at the right hand side */}
+            <div>
+                <StudentProfile />
+            </div>
+        </div>
+    );
+};
+
+export default StudentData;
