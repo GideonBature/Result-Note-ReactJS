a31035f16899281c10eae2b365df4fdcc2c6077e
wrightkhlebisol <calebogundiya@gmail.com>
2024-02-27 23:36:41 +0000

    Search form

--- src/components/students/StudentData.jsx
+++ src/components/students/StudentData.jsx
@@ -8,6 +8,9 @@ const StudentData = ({headings, contents}) => {
     return (
         <div className="flex">
             <div className="mr-8 flex-1 overflow-x-auto">
+                <form className="my-3 text-center justify-start w-full">
+                    <input required type="text" name="fullName" className="input p-1 w-full rounded-md border-gray-300 bg-gray-100 text-gray-700 text-md" placeholder='Search for a teacher by name or email'/>
+                </form>    
                 <Table headings={headings} contents={ contents} />
             </div>
             {/* Student profile at the right hand side */}
